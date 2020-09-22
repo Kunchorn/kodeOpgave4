5730d3a15c024e2064da0fce9da0f5eaa961ae00
Welander1994 <64834767+Welander1994@users.noreply.github.com>
2020-09-22 12:39:56 +0200

    changed css
    fixed
    - colors in burgermenu
    - position of imageslider / full and no full screen
    - smaller imager on gallery
    - fit landscape when in fullscreen
    - moved open / close button to right positions
    - added background image

--- js/javascript.js
+++ js/javascript.js
@@ -1,33 +1,35 @@
 console.log("javascript is running, get after it");
 
 /* Burgermenu start------------------------------------------------- */
+/*
 var wss_i = 0;
 var wss_array = ["Fyns", "Militærhistoriske", "Museum"];
 var wss_elem;
 function wssNext() {
-    wss_i++;
-    wss_elem.style.opacity = 0;
-    if (wss_i > (wss_array.length - 1)) {
-        wss_i = 0;
-    }
-    setTimeout('wssSlide()', 1000);
+  wss_i++;
+  wss_elem.style.opacity = 0;
+  if (wss_i > (wss_array.length - 1)) {
+    wss_i = 0;
+  }
+  setTimeout('wssSlide()', 1000);
 }
 function wssSlide() {
-    wss_elem.innerHTML = wss_array[wss_i];
-    wss_elem.style.opacity = 1;
-    setTimeout('wssNext()', 3000);
+  wss_elem.innerHTML = wss_array[wss_i];
+  wss_elem.style.opacity = 1;
+  setTimeout('wssNext()', 3000);
 }
 wss_elem = document.getElementById("wss"); wssSlide();
+*/
 
 function toggleNavPanel(x) {
-    var panel = document.getElementById(x), navarrow = document.getElementById("navarrow"), maxH = "600px"; /* max længde af container skal være større end Css value */
-    if (panel.style.height == maxH) {
-        panel.style.height = "0px";
-        navarrow.innerHTML = "&#9776";
-    } else {
-        panel.style.height = maxH;
-        navarrow.innerHTML = "&#9776";
-    }
+  var panel = document.getElementById(x), navarrow = document.getElementById("navarrow"), maxH = "600px"; /* max længde af container skal være større end Css value */
+  if (panel.style.height == maxH) {
+    panel.style.height = "0px";
+    navarrow.innerHTML = "&#9776";
+  } else {
+    panel.style.height = maxH;
+    navarrow.innerHTML = "&#9776";
+  }
 }
 
 /* Burgermenu slut---------------------------------- */
